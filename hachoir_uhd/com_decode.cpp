96df09744ca1ae34a52d5656e9edb0882d21fadd
Martin Peres <martin.peres@labri.fr>
2014-03-09 22:18:50 +0100

    hachoir_uhd: save the samples to disc for every burst

--- hachoir_uhd/com_decode.cpp
+++ hachoir_uhd/com_decode.cpp
@@ -26,6 +26,19 @@ void process_burst_sc16(burst_sc16_t *burst)
 		}
 	}
 
+	char filename[100];
+	sprintf(filename, "burst_%i.dat", burst->burst_id);
+	FILE *f = fopen(filename, "wb");
+	fwrite(burst->samples, sizeof(std::complex<short>), burst->len, f);
+	fclose(f);
+
+	sprintf(filename, "burst_%i.csv", burst->burst_id);
+	f = fopen(filename, "wb");
+	for (int i = 0; i < burst->len; i++) {
+		fprintf(f, "%i, %i\n", burst->samples[i].real(), burst->samples[i].imag());
+	}
+	fclose(f);
+
 	// bail out if the score is very low!
 	if (bestScore < 128) {
 		std::cerr << "Demod: burst ID "
@@ -46,20 +59,4 @@ void process_burst_sc16(burst_sc16_t *burst)
 	<< "BIN: " << m.toString(Message::BINARY) << std::endl
 	<< "HEX: " << m.toString(Message::HEX) << std::endl
 	<< std::endl;
-
-	/*char filename[100];
-	sprintf(filename, "burst_%i.csv", burst->burst_id);
-	FILE *f = fopen(filename, "wb");
-	for (int i = 0; i < burst->len; i++) {
-		fprintf(f, "%i, %i\n", burst->samples[i].real(), burst->samples[i].imag());
-	}
-	fclose(f);*/
-
-	/*char filename[100];
-	sprintf(filename, "burst_%i.dat", burst->burst_id);
-	FILE *f = fopen(filename, "wb");
-	fwrite(burst->samples, sizeof(std::complex<short>), burst->len, f);
-	fclose(f);*/
-
-
 }
