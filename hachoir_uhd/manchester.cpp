17f8f5b34d7e78bfe90576a2fe714470ab1ebefc
Martin Peres <martin.peres@labri.fr>
2014-03-13 02:36:42 +0100

    hachoir_uhd: fix the manchester code parsing

--- hachoir_uhd/manchester.cpp
+++ hachoir_uhd/manchester.cpp
@@ -5,7 +5,7 @@ bool Manchester::decode(const Message &in, Message &out)
 	if (in.size() % 2 != 0)
 		return false;
 
-	for(size_t i = 0; i < in.size(); i+=2) {
+	for(size_t i = 0; i < in.size() - 1; i+=2) {
 		if (in[i] == 0 && in[i + 1] == 1)
 			out.addBit(false);
 		else if (in[i] == 1 && in[i + 1] == 0)
