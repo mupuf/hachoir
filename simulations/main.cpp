08c4d5c50f711ee4e34ac64b0cd83779e0944916
Martin Peres <martin.peres@free.fr>
2014-08-31 23:53:36 +0200

    add another experiment

--- simulations/main.cpp
+++ simulations/main.cpp
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <math.h>
 
-uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod)
+uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod, size_t beaconCount)
 {
 	float band1 = 325e6 + (rand_cmwc() % 2650) * 1e6;
 	float band2 = 325e6 + (rand_cmwc() % 2650) * 1e6;
@@ -17,10 +17,10 @@ uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod)
 	HoppingPattern hp1(1000e3, Band(300e6, 3e9), 25e6, HoppingPattern::LINEAR, 1e3);
 	hp1.addAvailableEntry(Band(band1, band1+25e6), 0.0, 0.4);
 	hp1.addAvailableEntry(Band(band2, band2+25e6), 0.5, 0.4);
-	CogNode n1(42, 0, 0, beaconningPeriod, hp1, 1);
+	CogNode n1(42, 0, 0, beaconningPeriod, hp1, beaconCount);
 
 	HoppingPattern hp2(60e6, Band(300e6, 3e9), 25e6, HoppingPattern::RANDOM, hoppingPeriod);
-	CogNode n2(43, 0, 0, 10e3, hp2, 1);
+	CogNode n2(43, 0, 0, 10e3, hp2, beaconCount);
 
 	CogNode *nodes[] = { &n1, &n2 };
 	size_t nodesCount = sizeof(nodes) / sizeof(CogNode *);
@@ -50,7 +50,7 @@ uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod)
 	//std::cout << std::endl;
 }
 
-void scenario1() {
+void scenario1_periods_vs_avgtime() {
 	std::cout << "Hopping Period (µs), Beaconning Period (µs), average time (s), std, min, max" << std::endl;
 
 	for (size_t hp = 0; hp < 5; hp ++) {
@@ -60,7 +60,7 @@ void scenario1() {
 
 			uint64_t sum = 0, sum_sq = 0, count = 1000, min = -1, max = 0;
 			for (size_t i = 0; i < count; i++) {
-				uint64_t res = scenario1_run(hp_us, bp_us);
+				uint64_t res = scenario1_run(hp_us, bp_us, 1);
 				sum_sq += (res * res);
 				sum += res;
 				if (res < min)
@@ -77,11 +77,35 @@ void scenario1() {
 	}
 }
 
+void scenario1_beaconCount_vs_time() {
+	std::cout << "Beacon Count, Hopping Period (µs), Beaconning Period (µs), average time (s), std, min (s), max (s)" << std::endl;
+
+	size_t hp_us = pow(10, 1) * 1e3;
+	size_t bp_us = pow(10, 0) * 1e3;
+	for (size_t bc = 1; bc < 30; bc++) {
+		uint64_t sum = 0, sum_sq = 0, count = 1000, min = -1, max = 0;
+		for (size_t i = 0; i < count; i++) {
+			uint64_t res = scenario1_run(hp_us, bp_us, bc);
+			sum_sq += (res * res);
+			sum += res;
+			if (res < min)
+				min = res;
+			if (res > max)
+				max = res;
+		}
+		std::cout << bc << ", " << hp_us << ", " << bp_us << ", "
+			  << sum / count / 1.0e6 << ", "
+			  << sqrtf(sum_sq - (sum * sum)) << ", "
+			  << min / 1.0e6 << ", " << max / 1.0e6
+			  << std::endl;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	init_rand(time(NULL));
 
-	scenario1();
+	scenario1_beaconCount_vs_time();
 
 	return 0;
 }
