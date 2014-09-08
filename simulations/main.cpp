86cbc80a3a4d1037748e797e07c86ca5eca90e3a
Martin Peres <martin.peres@labri.fr>
2014-09-08 11:39:34 +0200

    simulation: delete the now-useless scenario2
    It only served my debugging purpose

--- simulations/main.cpp
+++ simulations/main.cpp
@@ -19,7 +19,7 @@ uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod, size_t
 	hp1.addAvailableEntry(Band(band2, band2+25e6), 0.5, 0.4);
 	CogNode n1(42, 0, 0, beaconningPeriod, hp1, beaconCount);
 
-	HoppingPattern hp2(60e6, Band(300e6, 3e9), 26e6, HoppingPattern::RANDOM, hoppingPeriod);
+	HoppingPattern hp2(60e6, Band(300e6, 3e9), 25e6, HoppingPattern::RANDOM, hoppingPeriod);
 	CogNode n2(43, 0, 0, 10e3, hp2, beaconCount);
 
 	CogNode *nodes[] = { &n1, &n2 };
@@ -58,7 +58,7 @@ void scenario1_periods_vs_avgtime() {
 			size_t hp_us = pow(10, hp) * 1e3;
 			size_t bp_us = pow(10, bp) * 1e3;
 
-			uint64_t sum = 0, sum_sq = 0, count = 1000, min = -1, max = 0;
+			uint64_t sum = 0, sum_sq = 0, count = 10000, min = -1, max = 0;
 			for (size_t i = 0; i < count; i++) {
 				uint64_t res = scenario1_run(hp_us, bp_us, 1);
 				sum_sq += (res * res);
@@ -83,7 +83,7 @@ void scenario1_beaconCount_vs_time() {
 	size_t hp_us = pow(10, 1) * 1e3;
 	size_t bp_us = pow(10, 1) * 1e3;
 	for (size_t bc = 1; bc <= 10; bc++) {
-		uint64_t sum = 0, sum_sq = 0, count = 1000000, min = -1, max = 0;
+		uint64_t sum = 0, sum_sq = 0, count = 100000, min = -1, max = 0;
 		for (size_t i = 0; i < count; i++) {
 			//std::cout << "Run scenard 1: " << i << "\n";
 			uint64_t res = scenario1_run(hp_us, bp_us, bc);
@@ -102,78 +102,11 @@ void scenario1_beaconCount_vs_time() {
 	}
 }
 
-uint64_t scenario2_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod, size_t beaconCount)
-{
-	float band1 = 300e6;
-
-	/*std::cout << "Band1: " << band1 / 1e6;
-	std::cout << ", Band2: " << band2 / 1e6 << std::endl;*/
-
-	HoppingPattern hp1(1000e3, Band(300e6, 3e9), 25e6, HoppingPattern::LINEAR, 1e3);
-	hp1.addAvailableEntry(Band(band1, band1+25e6), 0.0, 1.0);
-	CogNode n1(42, 0, 0, beaconningPeriod, hp1, beaconCount);
-
-	HoppingPattern hp2(60e6, Band(300e6, 3e9), 25e6, HoppingPattern::LINEAR, hoppingPeriod);
-	CogNode n2(43, 0, 0, 10e3, hp2, beaconCount);
-
-	CogNode *nodes[] = { &n1, &n2 };
-	size_t nodesCount = sizeof(nodes) / sizeof(CogNode *);
-
-	while (true) {
-		uint64_t next = -1;
-
-		for (size_t i = 0; i < nodesCount; i++) {
-			uint64_t nodeNext = nodes[i]->usUntilNextOperation();
-			if (nodeNext < next)
-				next = nodeNext;
-		}
-
-		//std::cout << "next = " << next << std::endl;
-
-		// FIXME? If a node jumps to a frequency, it won't be able to
-		// receive a message until the next tick change. That's pretty
-		// much what we get in real life though :s
-		for (size_t i = 0; i < nodesCount; i++) {
-			nodes[i]->addTicks(next);
-			if (n2.hasNeighbour(42)) {
-				return n2.ticksSinceStart();
-			}
-		}
-	}
-
-	//std::cout << std::endl;
-}
-
-void scenario2_test() {
-	size_t hp_us = pow(10, 1) * 1e3;
-	size_t bp_us = pow(10, 0) * 1e3;
-
-	for (size_t bc = 1; bc <= 5; bc++) {
-		uint64_t sum = 0, sum_sq = 0, count = 1, min = -1, max = 0;
-		for (size_t i = 0; i < count; i++) {
-			uint64_t res = scenario2_run(10e3, 10e3, bc);
-			sum_sq += (res * res);
-			sum += res;
-			if (res < min)
-				min = res;
-			if (res > max)
-				max = res;
-		}
-
-		std::cout << bc << ", " << hp_us << ", " << bp_us << ", "
-			  << sum / count / 1.0e6 << ", "
-			  << sqrtf(sum_sq - (sum * sum)) << ", "
-			  << min / 1.0e6 << ", " << max / 1.0e6
-			  << std::endl;
-	}
-}
-
 int main(int argc, char **argv)
 {
 	init_rand(time(NULL));
 
-	//scenario2_test();
-
+	//scenario1_periods_vs_avgtime();
 	scenario1_beaconCount_vs_time();
 
 	return 0;
