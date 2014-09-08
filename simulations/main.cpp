44c17f72ed434e8e953419f7495b75d878750ba5
Martin Peres <martin.peres@labri.fr>
2014-09-08 17:30:24 +0200

    simulation: add a new experiment that varies the radio's bandwidth

--- simulations/main.cpp
+++ simulations/main.cpp
@@ -6,7 +6,8 @@
 #include <time.h>
 #include <math.h>
 
-uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod, size_t beaconCount)
+uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod,
+		       size_t beaconCount, float senseBW)
 {
 	float band1 = 325e6 + (rand_cmwc() % 2650) * 1e6;
 	float band2 = 325e6 + (rand_cmwc() % 2650) * 1e6;
@@ -19,7 +20,7 @@ uint64_t scenario1_run(uint64_t hoppingPeriod, uint64_t beaconningPeriod, size_t
 	hp1.addAvailableEntry(Band(band2, band2+25e6), 0.5, 0.4);
 	CogNode n1(42, 0, 0, beaconningPeriod, hp1, beaconCount);
 
-	HoppingPattern hp2(60e6, Band(300e6, 3e9), 25e6, HoppingPattern::RANDOM, hoppingPeriod);
+	HoppingPattern hp2(60e6, Band(300e6, 3e9), senseBW, HoppingPattern::RANDOM, hoppingPeriod);
 	CogNode n2(43, 0, 0, 10e3, hp2, beaconCount);
 
 	CogNode *nodes[] = { &n1, &n2 };
@@ -60,7 +61,7 @@ void scenario1_periods_vs_avgtime() {
 
 			uint64_t sum = 0, sum_sq = 0, count = 10000, min = -1, max = 0;
 			for (size_t i = 0; i < count; i++) {
-				uint64_t res = scenario1_run(hp_us, bp_us, 1);
+				uint64_t res = scenario1_run(hp_us, bp_us, 1, 25e6);
 				sum_sq += (res * res);
 				sum += res;
 				if (res < min)
@@ -85,8 +86,7 @@ void scenario1_beaconCount_vs_time() {
 	for (size_t bc = 1; bc <= 10; bc++) {
 		uint64_t sum = 0, sum_sq = 0, count = 100000, min = -1, max = 0;
 		for (size_t i = 0; i < count; i++) {
-			//std::cout << "Run scenard 1: " << i << "\n";
-			uint64_t res = scenario1_run(hp_us, bp_us, bc);
+			uint64_t res = scenario1_run(hp_us, bp_us, bc, 25e6);
 			sum_sq += (res * res);
 			sum += res;
 			if (res < min)
@@ -102,12 +102,39 @@ void scenario1_beaconCount_vs_time() {
 	}
 }
 
+void scenario1_bandwidth_vs_time() {
+	std::cout << "Bandwidth, Beacon Count, Hopping Period (µs), Beaconning Period (µs), average time (s), std, min (s), max (s)" << std::endl;
+
+	size_t hp_us = pow(10, 1) * 1e3;
+	size_t bp_us = pow(10, 1) * 1e3;
+	size_t bc = 2;
+
+	for (float bandwidth = 4e6; bandwidth <= 100e6; bandwidth+=2e6) {
+		uint64_t sum = 0, sum_sq = 0, count = 1000000, min = -1, max = 0;
+		for (size_t i = 0; i < count; i++) {
+			uint64_t res = scenario1_run(hp_us, bp_us, bc, bandwidth);
+			sum_sq += (res * res);
+			sum += res;
+			if (res < min)
+				min = res;
+			if (res > max)
+				max = res;
+		}
+		std::cout << bandwidth << ", " << bc << ", " << hp_us << ", "
+			  << bp_us << ", " << sum / count / 1.0e6 << ", "
+			  << sqrtf(sum_sq - (sum * sum)) << ", "
+			  << min / 1.0e6 << ", " << max / 1.0e6
+			  << std::endl;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	init_rand(time(NULL));
 
 	//scenario1_periods_vs_avgtime();
-	scenario1_beaconCount_vs_time();
+	//scenario1_beaconCount_vs_time();
+	scenario1_bandwidth_vs_time();
 
 	return 0;
 }
